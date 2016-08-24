01691a82ed6614153f2686a221a97886de7dcb86
Andrew Armenante <andrew.armenante@gmail.com>
2016-08-24 15:07:50 -0400

    port issues

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import webpack from 'webpack';
 import middleware from './middleware';
 
 const app = express();
-PORT = process.env.listen(process.env.PORT || 8000)
+PORT = process.env.PORT || 8000
 
 
 if(process.env.NODE_ENV === 'development') {
